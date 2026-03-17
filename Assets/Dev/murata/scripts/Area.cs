c05293d54cc3db3b1a77aec324c0c160da4880c7
LianLuo-max <gyamnj@gmail.com>
2026-03-17 14:39:30 +0900

    ちゃんとしました

--- Assets/Dev/murata/scripts/Area.cs
+++ Assets/Dev/murata/scripts/Area.cs
@@ -1,9 +1,40 @@
+using TMPro;
 using UnityEngine;
 
 public class Area : MonoBehaviour
 {
-	[SerializeField] public Vector3[] CardPosition;
+	[SerializeField] private Vector3[] _cardPosition; // カードをセットする位置
 	[SerializeField] private float _setDistance = 1.0f; // セットを許容する最大距離
+
+	[SerializeField] private bool[] _isSet; // カードがセットされているかどうか 見るためにSerializeFieldに
+
+	public bool AllSet = false;
+
+	void Start()
+	{
+		// _cardPositionの数に合わせて配列を初期化
+		_isSet = new bool[_cardPosition.Length];
+	}
+
+	public void RemoveAria(GameObject card)
+	{
+		Vector3 cardPos = card.transform.position;
+		cardPos.y = 0;
+
+		for (int i = 0; i < _cardPosition.Length; i++)
+		{
+			// カードと現在の_cardPosition[i]の距離を計算
+			Vector3 areaPos = _cardPosition[i];
+			areaPos.y = 0;
+
+			if (cardPos == areaPos) // セットされているなら
+			{
+				_isSet[i] = false;
+				AllSet = false;
+				break;
+			}
+		}
+	}
 
 	public void SetAria(GameObject card)
 	{
@@ -11,11 +42,11 @@ public class Area : MonoBehaviour
 		int nearestIndex = -1; // 近い座標の配列番号
 		float maxDistance = _setDistance * _setDistance; // どこまで近づけたらセットされるかの距離
 
-		// CardPositionの中から引数のカードに一番近い座標を探す
-		for (int i = 0; i < CardPosition.Length; i++)
+		// _cardPositionの中から引数のカードに一番近い座標を探す
+		for (int i = 0; i < _cardPosition.Length; i++)
 		{
-			// カードと現在のCardPosition[i]の距離を計算
-			Vector3 diff = card.transform.position - CardPosition[i];
+			// カードと現在の_cardPosition[i]の距離を計算
+			Vector3 diff = card.transform.position - _cardPosition[i];
 			diff.y = 0; // yの差分を0に
 
 			float dis = diff.sqrMagnitude;
@@ -27,10 +58,10 @@ public class Area : MonoBehaviour
 			}
 		}
 
-		if (nearestIndex != -1 && nearestPos <= maxDistance) // 最も近い場所が_setDistance以内なら
+		if (nearestIndex != -1 && nearestPos <= maxDistance && !_isSet[nearestIndex]) // 最も近い場所が_setDistance以内でまだセットされていないなら
 		{
 			// 一番近かったインデックスのワールド座標を取得
-			Vector3 setPos = CardPosition[nearestIndex];
+			Vector3 setPos = _cardPosition[nearestIndex];
 
 			// Y座標は変更しない
 			setPos.y = card.transform.position.y;
@@ -38,11 +69,24 @@ public class Area : MonoBehaviour
 			// カードの位置をセット
 			card.transform.position = setPos;
 
-			Debug.Log($"{card.name} をポジション {nearestIndex} にセットしました");
-		}
-		else
-		{
-			Debug.Log("有効な範囲内にポジションが見つかりませんでした");
+			// フラグを立てる
+			_isSet[nearestIndex] = true;
+
+			// 全てセットしたか確かめるループ
+			bool checkAll = true;
+			for (int i = 0; i < _cardPosition.Length; i++)
+			{
+				if (!_isSet[i]) // falseなら
+				{
+					checkAll = false;
+					break;
+				}
+			}
+
+			AllSet = checkAll;
+
+			Debug.Log($"{card.name} を {nearestIndex} にセットしました");
+			if (AllSet) Debug.Log("すべてのカードがセットされました");
 		}
 	}
 }

767eaa72562601ea7820412fe8f44db920cd1fc3
LianLuo-max <gyamnj@gmail.com>
2026-03-18 15:07:28 +0900

    DrawCardの引数を追加、SetSoldierにフラグ等追加

--- Assets/Dev/kita/Script/SetSoldier.cs
+++ Assets/Dev/kita/Script/SetSoldier.cs
@@ -4,19 +4,23 @@ using UnityEngine.UI;
 
 public class SetSoldier : MonoBehaviour
 {
-    public int CardIndex;//セットしたい兵士の添え字を入力
+    public int CardIndex;  //セットしたい兵士の添え字を入力
 
-    
-    public int SoldierAtk;//セットしたい兵士のレベルを入力
+    public int SoldierAtk; //セットしたい兵士のレベルを入力
 
-    [SerializeField]
-    SoldierData soldierData;//兵士のデータベース
+	public int OwnerPlayer; // どちらのプレイヤーが持っているか
 
-    [SerializeField]
-    Image frontImage;//兵士の表の画像を表示するUI 
+	public bool IsGeneral; // 大将かどうか
 
     [SerializeField]
-    Image backImage;//兵士の裏の画像を表示するUI 
+    private SoldierData _soldierData;//兵士のデータベース
+
+    [SerializeField]
+	private Image _frontImage;//兵士の表の画像を表示するUI 
+
+    [SerializeField]
+	private Image _backImage;//兵士の裏の画像を表示するUI
+
     void Start()
     {
         if (CardIndex > 12) 
@@ -24,26 +28,18 @@ public class SetSoldier : MonoBehaviour
             Debug.LogError("兵士の添え字が大きすぎます。");
         }
 
-        SoldierAtk=soldierData.SoldierList[CardIndex].CardNum;//兵士のレベルをセット
+        SoldierAtk = _soldierData.SoldierList[CardIndex].CardNum;//兵士のレベルをセット
 
         Debug.Log("兵士の攻撃力は" + SoldierAtk + "です。");
 
         SetSprite();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void SetSprite() 
     {
-        if(!soldierData.SoldierList[CardIndex].CardBack) frontImage.sprite = soldierData.SoldierList[CardIndex].CardSprite;//兵士の画像をセット
-        else frontImage.sprite = soldierData.SoldierBack;//兵士の裏の画像をセット
+        if(!_soldierData.SoldierList[CardIndex].CardBack) _frontImage.sprite = _soldierData.SoldierList[CardIndex].CardSprite;//兵士の画像をセット
+        else _frontImage.sprite = _soldierData.SoldierBack;//兵士の裏の画像をセット
 
-        backImage.sprite = soldierData.SoldierBack;//兵士の裏の画像をセット
+        _backImage.sprite = _soldierData.SoldierBack;//兵士の裏の画像をセット
     }
-
 }

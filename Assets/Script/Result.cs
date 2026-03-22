a2e9f7f107d4d769d7cc91652cd970ec96a35c32
LianLuo-max <gyamnj@gmail.com>
2026-03-22 18:08:07 +0900

    a

--- Assets/Script/Result.cs
+++ Assets/Script/Result.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Result : MonoBehaviour
@@ -9,14 +12,51 @@ public class Result : MonoBehaviour
 	[SerializeField]
 	Image _lose;
 
+	[SerializeField] string _nextSceneName = "Title";
+	[SerializeField] float _waitTime = 2.0f;
+
+	[SerializeField] GameObject _scene;
+
+	private bool _canSkip = false;
 
 	void Start()
     {
 		GameObject judge = GameObject.Find("GameJudge");
 		bool b = judge.GetComponent<GameJudge>().PlayerWin();
-		if (judge != null) Destroy(judge);
-
+		
 		_win.enabled = b;
 		_lose.enabled = !b;
+
+		Destroy(judge);
+
+		StartCoroutine(WaitBeforeClick());
+	}
+
+	private IEnumerator WaitBeforeClick()
+	{
+		yield return new WaitForSeconds(_waitTime);
+
+		_canSkip = true;
+		Debug.Log("クリックで遷移可能になりました");
+	}
+
+	private void Update()
+	{
+		if (_canSkip && Input.GetMouseButtonDown(0))
+		{
+			StartCoroutine(sk());
+		}
+	}
+
+	private IEnumerator sk()
+	{
+		Instantiate(_scene);
+
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(_nextSceneName);
+		asyncLoad.allowSceneActivation = false;
+
+		yield return new WaitForSeconds(0.7f);
+
+		asyncLoad.allowSceneActivation = true;
 	}
 }
